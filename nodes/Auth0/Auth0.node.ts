12dafce3a1d8194907df49fc781fb938b9012303
John Drefahl <john@velobpa.com>
2026-04-02 17:22:52 -0500

    chore: update nodes/Auth0/Auth0.node.ts to v1.0.0

--- nodes/Auth0/Auth0.node.ts
+++ nodes/Auth0/Auth0.node.ts
@@ -52,6 +52,9 @@ import { actionOperations, actionFields } from './actions/action';
 import { executeActionOperation } from './actions/action/action.operations';
 import type { ActionOperation } from './utils/types';
 
+// Tickets resource (new from generated)
+import { executeTicketsOperations } from './tickets.operations';
+
 // Log licensing notice once on module load
 const LICENSING_NOTICE = `
 [Velocity BPA Licensing Notice]
@@ -118,6 +121,10 @@ export class Auth0 implements INodeType {
 						value: 'role',
 					},
 					{
+						name: 'Tickets',
+						value: 'tickets',
+					},
+					{
 						name: 'User',
 						value: 'user',
 					},
@@ -148,6 +155,145 @@ export class Auth0 implements INodeType {
 			// Action
 			...actionOperations,
 			...actionFields,
+			// Tickets (new operations from generated)
+			{
+				displayName: 'Operation',
+				name: 'operation',
+				type: 'options',
+				noDataExpression: true,
+				displayOptions: {
+					show: {
+						resource: ['tickets'],
+					},
+				},
+				options: [
+					{
+						name: 'Create Password Change Ticket',
+						value: 'createPasswordChangeTicket',
+						description: 'Create a password change ticket',
+						action: 'Create password change ticket',
+					},
+					{
+						name: 'Create Email Verification Ticket',
+						value: 'createEmailVerificationTicket',
+						description: 'Create an email verification ticket',
+						action: 'Create email verification ticket',
+					},
+				],
+				default: 'createPasswordChangeTicket',
+			},
+			{
+				displayName: 'User ID',
+				name: 'userId',
+				type: 'string',
+				required: true,
+				displayOptions: {
+					show: {
+						resource: ['tickets'],
+						operation: ['createPasswordChangeTicket', 'createEmailVerificationTicket'],
+					},
+				},
+				default: '',
+				description: 'The ID of the user for whom the ticket is being created',
+			},
+			{
+				displayName: 'Connection ID',
+				name: 'connectionId',
+				type: 'string',
+				required: false,
+				displayOptions: {
+					show: {
+						resource: ['tickets'],
+						operation: ['createPasswordChangeTicket'],
+					},
+				},
+				default: '',
+				description: 'The connection that provides the identity provider used to authenticate the user',
+			},
+			{
+				displayName: 'Email',
+				name: 'email',
+				type: 'string',
+				required: false,
+				displayOptions: {
+					show: {
+						resource: ['tickets'],
+						operation: ['createPasswordChangeTicket'],
+					},
+				},
+				default: '',
+				description: 'The user will receive the ticket using this email address',
+			},
+			{
+				displayName: 'Mark Email as Verified',
+				name: 'markEmailAsVerified',
+				type: 'boolean',
+				required: false,
+				displayOptions: {
+					show: {
+						resource: ['tickets'],
+						operation: ['createPasswordChangeTicket'],
+					},
+				},
+				default: false,
+				description: 'Whether to mark the email as verified after password change',
+			},
+			{
+				displayName: 'Include Email in Redirect',
+				name: 'includeEmailInRedirect',
+				type: 'boolean',
+				required: false,
+				displayOptions: {
+					show: {
+						resource: ['tickets'],
+						operation: ['createPasswordChangeTicket', 'createEmailVerificationTicket'],
+					},
+				},
+				default: false,
+				description: 'Whether to include the email address as part of the redirect URL',
+			},
+			{
+				displayName: 'TTL (seconds)',
+				name: 'ttlSec',
+				type: 'number',
+				required: false,
+				displayOptions: {
+					show: {
+						resource: ['tickets'],
+						operation: ['createPasswordChangeTicket', 'createEmailVerificationTicket'],
+					},
+				},
+				default: 432000,
+				description: 'Number of seconds for which the ticket is valid before expiration',
+			},
+			{
+				displayName: 'Client ID',
+				name: 'clientId',
+				type: 'string',
+				required: false,
+				displayOptions: {
+					show: {
+						resource: ['tickets'],
+						operation: ['createPasswordChangeTicket', 'createEmailVerificationTicket'],
+					},
+				},
+				default: '',
+				description: 'The client ID of the application the user will be redirected to after using the ticket',
+			},
+			{
+				displayName: 'Organization ID',
+				name: 'organizationId',
+				type: 'string',
+				required: false,
+				displayOptions: {
+					show: {
+						resource: ['tickets'],
+						operation: ['createPasswordChangeTicket', 'createEmailVerificationTicket'],
+					},
+				},
+				default: '',
+				description: 'The organization ID',
+			},
 		],
 	};
 
@@ -192,12 +338,15 @@ export class Auth0 implements INodeType {
 					case 'action':
 						result = await executeActionOperation.call(this, operation as ActionOperation, i);
 						break;
+					case 'tickets':
+						result = await executeTicketsOperations.call(this, items);
+						break;
 					default:
 						throw new Error(`The resource "${resource}" is not supported`);
 				}
 
 				returnData.push(...result);
-			} catch (error) {
+			} catch (error: any) {
 				if (this.continueOnFail()) {
 					returnData.push({
 						json: {
@@ -213,4 +362,110 @@ export class Auth0 implements INodeType {
 
 		return [returnData];
 	}
+}
+
+// Tickets operations implementation (new from generated)
+async function executeTicketsOperations(
+	this: IExecuteFunctions,
+	items: INodeExecutionData[],
+): Promise<INodeExecutionData[]> {
+	const returnData: INodeExecutionData[] = [];
+	const operation = this.getNodeParameter('operation', 0) as string;
+	const credentials = await this.getCredentials('auth0ManagementApi') as any;
+
+	for (let i = 0; i < items.length; i++) {
+		try {
+			let result: any;
+			const baseUrl = credentials.domain.includes('://') ? credentials.domain : `https://${credentials.domain}`;
+
+			switch (operation) {
+				case 'createPasswordChangeTicket': {
+					const userId = this.getNodeParameter('userId', i) as string;
+					const connectionId = this.getNodeParameter('connectionId', i) as string;
+					const email = this.getNodeParameter('email', i) as string;
+					const markEmailAsVerified = this.getNodeParameter('markEmailAsVerified', i) as boolean;
+					const includeEmailInRedirect = this.getNodeParameter('includeEmailInRedirect', i) as boolean;
+					const ttlSec = this.getNodeParameter('ttlSec', i) as number;
+					const clientId = this.getNodeParameter('clientId', i) as string;
+					const organizationId = this.getNodeParameter('organizationId', i) as string;
+
+					const body: any = {
+						user_id: userId,
+					};
+
+					if (connectionId) body.connection_id = connectionId;
+					if (email) body.email = email;
+					if (markEmailAsVerified) body.mark_email_as_verified = markEmailAsVerified;
+					if (includeEmailInRedirect) body.includeEmailInRedirect = includeEmailInRedirect;
+					if (ttlSec) body.ttl_sec = ttlSec;
+					if (clientId) body.client_id = clientId;
+					if (organizationId) body.organization_id = organizationId;
+
+					const options: any = {
+						method: 'POST',
+						url: `${baseUrl}/api/v2/tickets/password-change`,
+						headers: {
+							'Authorization': `Bearer ${credentials.accessToken}`,
+							'Content-Type': 'application/json',
+						},
+						body,
+						json: true,
+					};
+
+					result = await this.helpers.httpRequest(options) as any;
+					break;
+				}
+
+				case 'createEmailVerificationTicket': {
+					const userId = this.getNodeParameter('userId', i) as string;
+					const clientId = this.getNodeParameter('clientId', i) as string;
+					const organizationId = this.getNodeParameter('organizationId', i) as string;
+					const ttlSec = this.getNodeParameter('ttlSec', i) as number;
+					const includeEmailInRedirect = this.getNodeParameter('includeEmailInRedirect', i) as boolean;
+
+					const body: any = {
+						user_id: userId,
+					};
+
+					if (clientId) body.client_id = clientId;
+					if (organizationId) body.organization_id = organizationId;
+					if (ttlSec) body.ttl_sec = ttlSec;
+					if (includeEmailInRedirect) body.includeEmailInRedirect = includeEmailInRedirect;
+
+					const options: any = {
+						method: 'POST',
+						url: `${baseUrl}/api/v2/tickets/email-verification`,
+						headers: {
+							'Authorization': `Bearer ${credentials.accessToken}`,
+							'Content-Type': 'application/json',
+						},
+						body,
+						json: true,
+					};
+
+					result = await this.helpers.httpRequest(options) as any;
+					break;
+				}
+
+				default:
+					throw new Error(`The operation "${operation}" is not supported`);
+			}
+
+			returnData.push({
+				json: result,
+				pairedItem: { item: i },
+			});
+		} catch (error: any) {
+			if (this.continueOnFail()) {
+				returnData.push({
+					json: { error: error.message },
+					pairedItem: { item: i },
+				});
+			} else {
+				throw error;
+			}
+		}
+	}
+
+	return returnData;
 }
